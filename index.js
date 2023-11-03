d081916d27c60205248d11414d6d5225eb8e4efd
Gerson Borja <gerson.borja@ctdesarrollo-sdr.org>
2023-11-03 18:19:32 +0100

    links checked

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const form = document.querySelector(".form");
-const inforTarjetones = document.querySelector(".infoTarjetones");
+const filesForm = document.querySelector(".form-section");
+const inforTarjetones = document.querySelector(".info-tarjetones");
 
 const sede = document.getElementById("sede");
 const linkFinal = document.getElementById("linkFinal");
 const linkBanner = document.getElementById("linkBanner");
+const linkSede = document.getElementById("linkSede");
 const furriel = document.getElementById("furriel");
 const kw = document.getElementById("kw");
 const matomo = document.getElementById("matomo");
@@ -11,12 +13,32 @@ const pixel = document.getElementById("pixel");
 
 const input = document.getElementById("file");
 const selectedFiles = document.querySelector(".selectedFiles");
+const alertDom = document.querySelector(".alert");
 
 const currentTarjeton = document.createElement("div");
+let alert = "";
+let showInputForm = false;
+let showInputParams = true;
 
-inforTarjetones.addEventListener("submit", async (e) => {
+inforTarjetones.addEventListener("submit", (e) => {
     e.preventDefault();
+    if(
+        sede.value === "" ||
+        linkFinal.value === "" ||
+        linkBanner.value === "" ||
+        furriel.value === "" ||
+        kw.value === "" ||
+        matomo.value === "" ||
+        pixel.value === ""
+    ) {
+        alert = "Llena todos los campos antes de seguir";
+        alertDom.textContent = alert;
+    } else {
+        alertDom.style.display = "none";
 
+        showInputForm = true;
+        if(showInputForm) filesForm.classList = "form-section"
+    }
 })
 
 input.addEventListener("change", () => {
@@ -39,14 +61,40 @@ form.addEventListener("submit", async (e) => {
 function verifyLinks(file) {
     let links = file.getElementsByTagName("a");
 
-    let finalLink = links[0];
-    let banner = links[1];
-    let button = links[2];
-    let email = links[6];
+    let finalLinkFromParams = linkFinal.value.trim() + furriel.value.trim() + kw.value.trim() + matomo.value.trim()
+    let bannerLinkFromParams = linkBanner.value + furriel.value + kw.value + matomo.value
+    let emailLinkFromParams = linkSede.value + furriel.value + kw.value + matomo.value
 
-    console.log(finalLink)
-    console.log(banner)
-    console.log(button)
-    console.log(email)
+    let finalLink = links[0].href;
+    let banner = links[1].href;
+    let email = links[6].href;
 
+    let checkFinalLink = checkUrls(finalLinkFromParams, finalLink)
+    let checkBannerLink = checkUrls(bannerLinkFromParams, banner)
+    let checkEmailLink = checkUrls(emailLinkFromParams, email)
+
+    if(!checkFinalLink) {
+        console.log(
+            "Hay un error en este link: " + finalLink + 
+            "\nEl valor correcto es: " + finalLinkFromParams
+        );
+    }
+
+    if(!checkBannerLink) {
+        console.log(
+            "Hay un error en este link: " + banner + 
+            "\nEl valor correcto es: " + bannerLinkFromParams 
+        );
+    }
+
+    if(!checkEmailLink) {
+        console.log(
+            "Hay un error en este link: " + email + 
+            "\nEl valor correcto es: " + emailLinkFromParams 
+        );
+    }
+}
+
+function checkUrls(urlFromParams, urlFromFile) {
+    return urlFromParams === urlFromFile ? true : false;
 }
